8d7864515642f158c553983bbe29c896d836656f
pranjalikawale <pranjalikawale1578@gmail.com>
2020-07-04 08:06:38 +0530

    ADDED:To check for current user is unauthorize

--- test_spotify.py
+++ test_spotify.py
@@ -19,7 +19,17 @@ class TestSpotify():
         # Validate response headers and body contents, e.g. status code.
         assert response.status_code == 200
 
+    def test_get_current_user_check_status_code_equals_401(self):
+        url = 'https://api.spotify.com/v1'
 
+        headers = {
+            'authorization': 'Bearer BQA8zZrYeKtkDEox5BN-U1iqdMsZeIcBlZkBmoTTmUl_pjgn1PnDucUQ9b8cm6BrexH-tl6hMNLBilpmJdSpq0xJ6qjxCKfygxAco_FSV50yAhMV4IadA6XAJJQSkPp-noRgnupd_x0ZuvusY1-OcTHL3LxtTT9R-TeU0Mq4eilj--P-vKaS2B3SZh72KAhCW0GFvZamspmZiRGaddvVIzQeXUM0iQrNYUECFVCsdA3v9KLJnSxqp9QsSTeoeFNGbLCuogQ2pZfTm7si0dr0JIuwpLT5a6_S',
+            'cache-control': 'no-cache'
+        }
+
+        response = requests.get( url, headers=headers)
+        # Validate response headers and body contents, e.g. status code.
+        assert response.status_code == 401
 
 
 
