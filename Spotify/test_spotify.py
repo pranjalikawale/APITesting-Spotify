e163b57ba5b98ef89050c3bf34bb1f461e8a78e9
pranjalikawale <pranjalikawale1578@gmail.com>
2020-07-06 15:17:01 +0530

    ADDED:To get user_id

--- Spotify/test_spotify.py
+++ Spotify/test_spotify.py
@@ -1,23 +1,39 @@
 import requests
 import json
+import pytest
 
 class TestSpotify():
+    token=""
+    playlists=[]
+    total_playlist=0
+    track_id=[]
+    user_id = ""
+    JSON = "application/json"
+
+    @pytest.fixture
+    def spotify_token(self):
+        token='Bearer BQChPQOx6YvcnUD9EWIwQIlNctTa9jAcF8hVpxo24CMB73x6DBa-IsWYO_CC7vId4WeyNQtcnGZpO_JkQHZVvgbxjfr1ZOD6Y19elLDideYURmwGvvvYqZITlzNGuQwEnVI1ulm_50tKTKbVtEGMscQApsbRHS5l5YhpjI_SIxxLr2AVyzxxN5Y8QN4GDpE9-eFT1S3UBDypvPOYtOD1NhUg-AP4yKJE6QMsdQ1ePBklZ0LboHVT3BhBtbKpPE9NwXxhWAVLl4DZzK_NKWuVf9qZoohBvAI3'
+        return token
 
     def test_given_valid_url_check_status_code_equals_200(self):
         response = requests.get("https://api.spotify.com")
         assert response.status_code == 200
-
-    def test_get_current_user_check_status_code_equals_200(self):
+       
+    def test_get_user_id(self,spotify_token):
         url = 'https://api.spotify.com/v1/me'
 
         headers = {
-            'authorization': 'Bearer BQCRXjNz1ueEuNsyBRih-O1JEtWDc9_6T47v2YDPNlehfh2EcRi5Olcc3pGs0HeHIB0kvJ-VNf-P-HEHljHsl0u_5oyaaDoAcxFmEawOfpVWWeD9mGr3prf9WTJYhJjr8nuOuD_bqxJvtIgH3vjAPnwHlnS0hyBLNjegOiuyqI19pfK5PVzreBVIS5aGkelYleIqh_C9FzzSTGP23acDVV59VcRHjJurhEbxir8oH_bqUR_5U7yYdtYYI7pNQ-vqy4lYt2NCneClyF_kVhbVcD5HVkU3vZGv',
-            'cache-control': 'no-cache'
+            'authorization': spotify_token ,
+            'cache-control': 'no-cache',
+            'Content-Type':'application/json'
         }
 
         response = requests.get( url, headers=headers)
         # Validate response headers and body contents, e.g. status code.
-        assert response.status_code == 200
+        response_body=response.json()
+        user_id=response_body['id']
+        assert user_id == 'y4qylmcub7wbtqwg9pqyyz652' and response.status_code == 200
+        print(user_id)
 
     def test_get_current_user_check_status_code_equals_401(self):
         url = 'https://api.spotify.com/v1'
